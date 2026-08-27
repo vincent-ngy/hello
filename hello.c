de5fcbf69b10c51db21644be41e3b863c92ace42
vincent-ngy <vnguyen61@huskers.unl.edu>
2026-08-27 09:38:09 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Vincent Nguyen
+ *         vnguyen61@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!");
+
+  return 0;
+}
