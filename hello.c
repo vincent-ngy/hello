07954bcb1de4cabb147e720600c1f1b269567edb
JoseManuel-1 <Jlarapatino3@huskers.unl.edu>
2026-08-27 10:59:35 -0500

    added name and major to parterns

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Hello World, My name is Vincent!\n");
   printf("I'm majoring in Computer Science\n");
+  printf("Hello world, i'm Jose Manuel!\n");
+  printf("my major is chemical engineering!\n");
 
   return 0;
 }
